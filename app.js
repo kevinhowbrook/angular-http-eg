7610511c41462dd4d0177b3fee365c246aabb704
Kevin Howbrook <kevinhowbrook@me.com>
2016-01-31 18:16:38 +0000

    update

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var app = angular.module('myApp', ['ngRoute']);
+var app = angular.module('myApp', ['ngRoute' , 'ui.bootstrap']);
 
 app.config(function ($routeProvider) {
     $routeProvider
@@ -10,12 +10,28 @@ app.config(function ($routeProvider) {
          templateUrl: './partials/node.html',
          controller: 'nodeCtrl'
      })
+    
      .otherwise({
          redirectTo: '/'
      });
 });
 
 app.controller('myCtrl', function($scope, $http) {
+	$scope.totalItems = 64;
+  	$scope.currentPage = 4;
+
+  $scope.setPage = function (pageNo) {
+    $scope.currentPage = pageNo;
+  };
+
+  $scope.pageChanged = function() {
+    $log.log('Page changed to: ' + $scope.currentPage);
+  };
+
+  $scope.maxSize = 5;
+  $scope.bigTotalItems = 175;
+  $scope.bigCurrentPage = 1;
+
   $http({
     method : "GET",
     url : "http://dev-kevinsandbox.pantheon.io/api/node"
@@ -33,9 +49,12 @@ app.controller('nodeCtrl', function($scope, $routeParams, $http, $sce) {
     url : "http://dev-kevinsandbox.pantheon.io/api/node/" + $routeParams.id
   	}).then(function mySucces(response) {
       $scope.title = response.data.title;
+      $scope.nid = response.data.nid;
       $scope.body = $sce.trustAsHtml(response.data.body);
+      $scope.categories = response.data.categories;
     }, function myError(response) {
       $scope.node = 'loading';
   });
+  });
 
-});
+
